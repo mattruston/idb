28c489e628124ddf740a4ea2e7986a45747cd3c0
Matt Ruston <matt@rustonmail.com>
2017-09-24 16:09:36 -0500

    Linked styles and other pages

--- app.py
+++ app.py
@@ -5,6 +5,26 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route("/about")
+def about():
+	return render_template('/about/index.html')
+
+@app.route("/games")
+def games():
+	return render_template('/games/index.html')
+
+@app.route("/developers")
+def developers():
+	return render_template('/developers/index.html')
+
+@app.route("/platforms")
+def platforms():
+	return render_template('/platforms/index.html')
+
+@app.route("/characters")
+def characters():
+	return render_template('/characters/index.html')
+
 
 if __name__ == "__main__":
     app.run()
