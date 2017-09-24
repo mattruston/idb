f30bdf475addfcff7d6b1978d1a3abc7f374202d
Matt Ruston <matt@rustonmail.com>
2017-09-24 16:11:19 -0500

    Convert to spaces

--- app.py
+++ app.py
@@ -7,23 +7,23 @@ def main():
 
 @app.route("/about")
 def about():
-	return render_template('/about/index.html')
+    return render_template('/about/index.html')
 
 @app.route("/games")
 def games():
-	return render_template('/games/index.html')
+    return render_template('/games/index.html')
 
 @app.route("/developers")
 def developers():
-	return render_template('/developers/index.html')
+    return render_template('/developers/index.html')
 
 @app.route("/platforms")
 def platforms():
-	return render_template('/platforms/index.html')
+    return render_template('/platforms/index.html')
 
 @app.route("/characters")
 def characters():
-	return render_template('/characters/index.html')
+    return render_template('/characters/index.html')
 
 
 if __name__ == "__main__":
