b7e033ffd310b1ce2fd29787eb864a5bd800c628
KishanRPatel <kishanrpatel@utexas.edu>
2017-10-20 12:32:01 -0500

    Setup Routing and hacked together Nav and footer

--- vgdb-frontend/src/App.js
+++ vgdb-frontend/src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch } from 'react-router-dom';
 import './App.css';
+
 import GridItem from './GridItem';
+import Nav from './Nav'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+				<Nav/>
         <GridItem details={['hi', 'there']}></GridItem>
+				<Switch>
+				{/*	
+					<Route exact path='/' 											component={Home}/>
+					<Route exact path='/games'									component={GamesPage}/>
+					<Route exact path='/developers'							component={DevelopersPage}/>
+					<Route exact path='/platforms'							component={PlatformsPage}/>
+					<Route exact path='/characters'							component={CharactersPage}/>
+					<Route exact path='/about'									component={AboutPage}/>
+					<Route exact path='/games/:game'						component={Game}/>
+					<Route exact path='/developers/:developer'	component={Developer}/>
+					<Route exact path='/platforms/:platform'		component={Platform}/>
+					<Route exact path='/characters/:character'	component={Character}/>
+				*/}	
+				</Switch>
+				<div className="footer">
+					<a class="link" href="https://github.com/mattruston/idb">Check out the project on Github</a>
+				</div>
       </div>
     );
   }
--- vgdb-frontend/src/Nav.js
+++ vgdb-frontend/src/Nav.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
+import './App.css';
 import NavItem from './NavItem';
 
 /* Nav component for a standard style navbar */
 class Nav extends Component {
     render() {
         return (
-            <div className="nav">
+            <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark justify-content-between">
+			<div class="container">
+				<a class="navbar-brand primary-text" href="/">gamingdb</a>
+				
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+					<form class="d-flex flex-row nav-input-group">
+						<input class="form-control nav-input" type="text" placeholder="Search" aria-label="Search"/>
+					    <button class="btn nav-input-button" type="submit">Search</button>
+					</form>
+					<div class="d-inline-flex flex-row justify-content-end w-100">
+						<NavItem text="Games" link="/games"/>
+                        <NavItem text="Developers" link="/developers"/>
+                        <NavItem text="Platforms" link="/platforms"/>
+                        <NavItem text="Characters" link="/characters"/>
+                        <NavItem text="About" link="/about"/>
+					</div>
+				    
+				</div>
+			</div>
+		</nav>
+            /*<div className="nav">
                 <div className="container">
                     <div className="nav-start">
                         <div className="nav-logo"></div>
                     </div>
                     <div className="nav-end">
                         <div className="nav-menu">
-                            <NavItem text="" link=""></NavItem>
-                            <NavItem text="" link=""></NavItem>
-                            <NavItem text="" link=""></NavItem>
+                            <NavItem text="Games" link="/games"/>
+                            <NavItem text="Developers" link="/developers"/>
+                            <NavItem text="Platforms" link="/platforms"/>
+			    <NavItem text="Characters" link="/characters"/>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>*/
         );
     }
 }
 
 export default Nav;
+
+
