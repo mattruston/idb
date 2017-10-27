ae819e0c8ba84e24f826f332ae6bfc30c62dd4a8
Jared Jones <jkjones.dev@gmail.com>
2017-10-26 19:35:37 -0500

    Adding about page information

--- vgdb-frontend/src/pages/About.js
+++ vgdb-frontend/src/pages/About.js
@@ -24,6 +24,7 @@ class About extends Component {
             method: 'GET'
         }).then(response => response.json())
         .then(response => {
+            console.log(response);
             for (var i = 0; i < response.length; i++) {
                 var json = { };
                 json[response[i].author.login] = response[i].total;
@@ -35,7 +36,9 @@ class About extends Component {
     render() {
         return (
             <div className="container main-page">
-                 <Title title="About"/>
+                <Title title="About"/>
+                <h3>Motivation</h3>
+                <p>Video games are a beloved childhood pastime that most people continue to enjoy well into adulthood. As a team, each of us grew up playing video games. Video games not only built friendships, but were a great way to experience a rich story with deep characters. There is nothing quite like getting a new game and playing it for the first time. Immersing yourself in a new game is an amazing feeling, but it can be hard to find new video games that you like. We created GamingDB so that people have a place to go to find a new game to play by providing information about everything that comes together to make a video game. From the developers who created the game to the major characters in the game, our site will provide users all the information they need about every aspect of a video game. We will provide information about video games, gaming platforms, major characters, and companies that develop video games. With these models, we will be able to create a great web of information that people can browse through whenever they want to find a new game to play.</p>
                 <div className="about-grid">
                     <div className="about-card-wrapper">
                         <AboutCard 
@@ -58,7 +61,24 @@ class About extends Component {
                         name="Matt Ruston" description="Hi, I'm Matt. I'm a senior CS student at UT." roles="Backend" commits={this.state.mattruston}  issues="15" tests="0" url="https://github.com/mattruston" image="https://avatars0.githubusercontent.com/u/10778569?s=400&v=4"/>
                     </div>
                 </div>
-                <ToolCard/>
+                <ToolCard title="Tools" list={[
+                    {name: "Trello", description:"Issue tracker"},
+                    {name: "PlanItPoker", description:"Creating and estimating user stories"},
+                    {name: "Google Domains", description:"Bought and registered our domain"},
+                    {name: "Apiary", description:"API Documentation"},
+                    {name: "Google Cloud Platform", description:"Used App Engine to deploy our site"},
+                    {name: "Slack", description:"Used for team communication"},
+                    {name: "Flask", description:"Python server engine to power all the api and forward all non-api routing to React"},
+                    {name: "React", description:"Frontend engine to power all of our clientside rendering and routing"}
+                    ]}/>
+                <ToolCard title="Stats" list={[
+                    {name: "API Docs", description: "Apiary", url:"http://docs.vgdb1.apiary.io/#"},
+                    {name: "Issue Tracker", description: "Trello", url:"https://trello.com/b/OBZSfuJU/idb"},
+                    {name: "Repository", description: "Github", url:"http://github.com/mattruston/idb"},
+                    {name: "Total Commits", description: this.state.Jaredk3nt + this.state.jmpo1618 + this.state.derektolliver + this.state.KishanRPatel + this.state.mattruston },
+                    {name: "Issues", decription: "36" },
+                    {name: "Unit Tests", description: "10" }
+                ]}/>
             </div>
         )
     }
