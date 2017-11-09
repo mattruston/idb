8b679205ca04afacbc6c4c03c3390dae9310a1c3
Jared Jones <jkjones.dev@gmail.com>
2017-11-08 20:38:12 -0600

    adding search item

--- vgdb-frontend/src/components/SearchItem.js
+++ vgdb-frontend/src/components/SearchItem.js
@@ -1,14 +1,66 @@
 import React, {Component} from 'react';
 
+const attr = ["description", "genre", "title"]
+
 class SearchItem extends Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            context: "",
+            searchTerms: this.props.query.split(" ")
+        }
     }
 
     render() {
         return (
-            <div></div>
+            <a className="search-item-container">
+                <img className="search-item-thumbnail" src={this.props.obj.image_url}/><div className="search-item-content">
+                    <div className="search-item-title">{this.props.obj.name}</div>
+                    <hr/>
+                    { this.contextFinder().map( (i) => i ) }
+                </div>
+            </a>
         );
+    }
+
+    contextFinder = () => {
+        let context = [];
+        let desc = this.props.obj.description.split(" ");
+        for(let i in desc) {
+            for(let term of this.state.searchTerms) {
+                if(desc[i].toLowerCase() === term.toLowerCase()) {
+                    let surroundFront = [];
+                    let surroundBack = [];
+                    let index = Math.max(0, parseInt(i) - 5);
+                    for(index; index < i; index++) {
+                        surroundFront.push(desc[index]);
+                    }
+                    index = parseInt(i) + 1;
+                    console.log("index: " + index + " desc: " + desc.length);
+                    for(index; index < desc.length && index < (parseInt(i) + 5); index++) {
+                        console.log(desc[index]);
+                        surroundBack.push(desc[index]);
+                    }
+                    context.push(
+                        <div className="search-item-description">
+                            ...{surroundFront.join(' ')} <strong>{desc[i]}</strong> {surroundBack.join(' ')}... 
+                        </div>
+                    );
+                }
+            }
+            if(context.length > 2) {
+                break;
+            }
+        }
+        if(context.length < 1) {
+            context.push(
+                <div className="search-item-description">
+                    {this.props.obj.description.substring(0, 100)}...
+                </div>
+            )
+        }
+        return context;
     }
 }
 
--- vgdb-frontend/src/pages/Search.js
+++ vgdb-frontend/src/pages/Search.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import SearchItem from '../components/SearchItem';
+import Title from '../components/Title';
+import Loader from '../components/Loader';
+import './styles/search.css';
 
 const endpoint = (query, type) => {
     return `http://gamingdb.info/api/${type}?q={"filters":[{"or":[{"name":"name","op":"ilike","val":"${query}"},{"name":"description","op":"ilike","val":"${query}"},{"name":"genres","op":"any","val":{"name":"name","op":"ilike","val":"${query}"}}]}]}`;
@@ -14,16 +17,28 @@ class Search extends Component {
             devResults: [],
             platformResults: [],
             characterResults: [],
-            loading: true
+            loading: false
         }
     }
 
     render() {
         return (
             <div>
-                { this.state.gameResults.map( () => {
-                    <SearchItem/>
-                })}
+                {this.state.loading && <Loader/>}
+                {!this.state.loading && 
+                    <div className="container main-page">
+                        <div className="search-query">Search Query: <strong>{this.props.match.params.query}</strong></div>
+                        <SearchItem obj={ 
+                            { name: "FIFA 18", description: "Powered by Frostbite™, EA SPORTS™ 18 blurs the line between the virtual and real worlds, bringing to life the players, teams, and atmospheres that immerse you in the emotion of The World’s Game.", 
+                                image_url:"http://res.cloudinary.com/gamingdb/image/upload/games/sdvrxyue8h4vg3ojihfb.jpg"} } query="fifa soccer"/>
+                        <SearchItem obj={ 
+                            { name: "FIFA 18", description: "Powered by Frostbite™, EA SPORTS™ FIFA 18 blurs the line between the virtual and fifa real worlds, bringing to life fifa the players, teams, and atmospheres that immerse you in the emotion of fifa The World’s Game.", 
+                                image_url:"http://res.cloudinary.com/gamingdb/image/upload/games/sdvrxyue8h4vg3ojihfb.jpg"} } query="fifa soccer"/>
+                        { this.state.gameResults.map( () => {
+                            <SearchItem/>
+                        })}
+                    </div>
+                }
             </div>
         );
     }
