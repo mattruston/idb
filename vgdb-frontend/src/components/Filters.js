c8888ee7449091f199141f8b80f2c2c89558b7d8
Kishan Patel <kpatel@cs.utexas.edu>
2017-11-18 17:16:38 -0600

    Improved closing dropdowns for sorting and filtering

--- vgdb-frontend/src/components/Filters.js
+++ vgdb-frontend/src/components/Filters.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import FilterInput from './FilterInput';
 
 class Filters extends Component {
@@ -8,6 +9,20 @@ class Filters extends Component {
 
         this.state = {
             drop: false
+        }
+    }
+
+     componentWillMount() {
+        document.addEventListener('click', this.handleClick, false);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleClick, false);
+    }
+
+    handleClick = e => {
+        if(!ReactDOM.findDOMNode(this).contains(e.target)) {
+            this.setState({drop: false});
         }
     }
 
