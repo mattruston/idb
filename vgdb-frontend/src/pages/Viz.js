a51e2a252c37eb3050e7dca96b43f74e239d5dba
Jared Jones <jkjones.dev@gmail.com>
2017-11-26 15:46:42 -0600

    temp commit for fetch logic

--- vgdb-frontend/src/pages/Viz.js
+++ vgdb-frontend/src/pages/Viz.js
@@ -2,6 +2,19 @@ import React, {Component} from 'react';
 import {Pie, PieChart, Cell, BarChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, Bar} from 'recharts';
 
 class Viz extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            modelsPieData = [];
+        }
+    }
+
+    componentDidMount() {
+        this.buildModelsPie();
+    }
+
     render() {
         const width = 400, height = 300;
         const PieData = [
@@ -19,7 +32,7 @@ class Viz extends Component {
                 <div className="graph-container">
                     <span className="graph">
                         <PieChart width={width} height={height}>
-                            <Pie data={PieData} dataKey="value" cx={width/2} cy={height/2} outerRadius={80} label={(obj)=> obj.name}>
+                            <Pie data={this.state.modelsPieData} dataKey="value" cx={width/2} cy={height/2} outerRadius={80} label={(obj)=> obj.name}>
                                 { PieData.map((entry, index) => <Cell fill={COLORS[index % COLORS.length]}/>) }
                             </Pie>
                         </PieChart>
@@ -48,6 +61,45 @@ class Viz extends Component {
                 </div>
             </div>
         );
+    }
+
+    buildModelsPie = () => {
+        fetch(
+            endpoint(this.props.match.params.page, 
+                JSON.stringify(this.state.filter), 
+                JSON.stringify(this.state.sort)),
+            { method: 'GET' })
+        .then(response => {
+            if(response.ok) {
+                return response.json();
+            }
+            throw new Error('Failed to retrieve response object for game.');
+        })
+        .then(response => {
+            this.setState({
+                pageLimit: response.total_pages
+            });
+            for (var i = 0; i < response.objects.length; i++) {
+                let obj = response.objects[i];
+                let details = this._buildDetails(obj);
+                let item = {
+                    name: obj.name,
+                    img: obj.thumb_url,
+                    url: "/games/" + obj.game_id,
+                    details: details
+                }
+                var gamesArray = this.state.games.slice();
+                gamesArray.push(item);
+                this.setState({ games: gamesArray });
+            }
+            this.setState({
+                loading: false
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    };
     }
 }
 
